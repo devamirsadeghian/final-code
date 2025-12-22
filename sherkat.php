4a117ef10a3a267766bcf14d9e8d2b522194ace0
Amirhossein Sadeghian <dev.amir.sadeghian@gmail.com>
2025-12-22 16:56:17 +0330

    version 2

--- sherkat.php
+++ sherkat.php
@@ -1,2 +1,11 @@
-// fsd
+<?php
+
+
+
+هیچ تاثیریر ندارد  attribute  ست میکنی  value  مقدار  attribute  وقتی به جای  gridshow  ها blade  در 
+بگیریم  composer و محتویات یکیج را در ان فایل  حذف کنیم و سپس vendor  باید بعد از حذف پوشه blade  مقداری را میخونه ، پس برای پاک  کردن composer json  فایل  composer install در زمان
+
+پیدا کن  url  کمک بگیر و در  url  یک چیزی را که نمیدانیم از  view , controller , route  برای پیدا کردن
+
+
 
